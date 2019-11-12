4600a71fc4407baa9dc26bc1957442252969fd3e
Esmail Shabayek <esmail.shabayek@gmail.com>
2019-11-12 15:19:04 +0200

    Initial connection to mongoDb

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $collection) {
+            $collection->string('name');
+            $collection->unique('email');
+            $collection->timestamp('email_verified_at')->nullable();
+            $collection->string('password');
+            $collection->rememberToken();
+            $collection->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
